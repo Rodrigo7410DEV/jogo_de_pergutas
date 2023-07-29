eab6baa979c23c735da915afe7ea7f87e50372c4
Rodrigo7410DEV <134988462+Rodrigo7410DEV@users.noreply.github.com>
2023-07-29 00:00:39 -0300

    Add files via upload

---
+++ Jogo_de_respostas.py
@@ -0,0 +1,59 @@
+#quastão 1
+
+reposta_1 = input("quem descobriu o brasil? ")
+
+if reposta_1 == "Pedro Àlvares Cabral":
+    print("Certa Responta")
+    print(end="\n")
+
+else:
+    print("Resposta incorreta")
+    print("resposta correta é Pedro Alvares Cabral")
+    print(end="\n")
+
+#Questão 2
+
+resposta_2 = input("Qual é a formula da água: \n(A)H3O \n(B)OH2 \n(C)H20 \n(D)H2O \n Qual é a formula: ")
+
+if resposta_2 == "D" or resposta_2 == "d":
+     print("Certa Responta")
+     print(end="\n")     
+
+else:
+    print("Resposta incorreta")
+    print("Correta (D) H2O ")
+    print(end="\n")
+
+# Questão 3
+resposta_3 = input("Onde o Brasil esta localizado? ")
+
+if resposta_3 == "Amercia do Sul" or resposta_3 == "Sul" or reposta_1 == "amercia do Sul" or resposta_3 == "sul":
+    print("Certa resposta")
+    print(end="\n")
+
+else:
+    print("Resposta incorreta")
+    print("Resposta correta é America do sul")
+    print(end="\n")
+
+#Questão 4
+
+resposta_4 = input("qual é o maior oceano do mundo? ")
+
+if resposta_4 == "Oceano Pacífoco" or resposta_4 == "oceano pacífoco":
+    print("Certa resposta")
+    print(end="\n")
+
+else:
+    print("Resposta incorreta")
+    print(end="\n")
+
+#Questão 5
+
+resposta_5 = input("Quem foi Marking Zunkenberg? ")
+
+if resposta_5 == "Uns dos maiores Programadores do mundo que crio o FaceBook, Instagram e outras redes sociais" or resposta_5 == "Criador do FaceBook e Instagram" or resposta_5 == "criador do faceBook e instagram":
+   print("Certa resposta")
+
+else:
+    print("Resposta incorrerta") 
